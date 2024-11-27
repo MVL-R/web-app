e607c44ec2e8eb1d2c730d8e99d3d73e2e1c6fdf
MVL-R <rodulfamarkvincent@gmail.com>
2024-11-27 21:45:58 +0800

    Server.js for the entry point for the website

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+
+const authRoutes = require('./src/routes/authRoutes');
+const postRoutes = require('./src/routes/postRoutes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'reddit-secret',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(express.static('public'));
+
+app.get('/register.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/posts', (req, res) => {
+  if (!req.session.user) {
+    return res.redirect('/login.html');
+  }
+  res.sendFile(path.join(__dirname, 'public', 'posts.html'));
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api', postRoutes);
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
