e421764ad68367c9d037b2d5c3e7d18242114d50
MVL-R <rodulfamarkvincent@gmail.com>
2024-12-05 22:13:28 +0800

    Delete src/routes/postRoutes.js
    File not needed anymore

--- src/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postController = require('../controllers/postController');
-
-router.get('/posts', postController.getAllPosts);
-router.post('/posts', postController.addPost);
-router.post('/posts/:id/upvote', postController.upvotePost);
-
-module.exports = router;
